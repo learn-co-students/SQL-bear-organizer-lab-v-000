62dea86be617bb78ae8059c8f3921e17a8834ebb
Dakota Martinez <dakotaleemusic@gmail.com>
2016-01-13 01:13:20 +0000

    completes SQL bear organizer lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Dark", "Hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "Energetic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Gray", "Shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "F", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "M", "Green", "Grouchy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "F", "Silver", "Motherly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "M", "Blackish-Gray", "Murderous", 1);
