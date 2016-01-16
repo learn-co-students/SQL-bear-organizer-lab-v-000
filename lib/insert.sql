553a9ea5a84b5375b9adddb36851d4a1c5dfb568
Marco Tsui <marcotsui2003@yahoo.com>
2016-01-16 12:18:58 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender) VALUES("Mr. Chocolate",4,'F');
+INSERT INTO bears (name,age,gender) VALUES("Rowdy",6,"M");
+INSERT INTO bears (name,age,gender) VALUES("Tabitha",1 ,"F");
+INSERT INTO bears (name,age,gender) VALUES("Sergeant Brown", 7, 'F');
+INSERT INTO bears (name,age,gender) VALUES("Melissa", 9, 'M');
+INSERT INTO bears (name,age,gender) VALUES("Grinch", 3, 'M');
+INSERT INTO bears (name,age,gender) VALUES("Wendy", 4, 'F');
+INSERT INTO bears (name,age,gender) VALUES(NULL,5,'M');
