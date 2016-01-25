93fbea7dab85627f48790032ae98ba5f500f5287
Tsundu <osmentdan@gmail.com>
2016-01-25 09:16:07 -0500

    added sqlite3 table, data and queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 22, "M", 'Brown', 'furious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, "M", 'White', 'curious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 13, "F", 'Brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergrant Brown', 55, "M", 'Brown', 'Angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 40, "F", 'White', 'Happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 80, "M", 'Green', 'Angry', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 4, "F", 'Black', 'Angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, "M", 'Brown', 'Calm', 0);
