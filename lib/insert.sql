0232c9bc6bd9472be508dc4ae025bbb8ee6381ab
Adam King <adam.king0126@gmail.com>
2016-02-04 13:55:26 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears (name, age, gender, color, temperament, alive) values ('Mr. Chocolate', 15, 'M', 'brown', 'mean', 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Peanut', 4, 'F', 'black', 'friendly', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Happy', 2, 'M', 'brown', 'standoffish', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Dopey', 19, 'M', 'brown', 'churlish', 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Charles', 13, 'M', 'brown', 'distant', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Gregory', 3, 'M', 'brown', 'affable', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Padington', 20, 'M', 'brown', 'manipuative', 1);
+insert into bears (name, age, gender, color, temperament, alive) values (NULL , NULL, 'M', 'brown', 'mean', 1);
+
