2d5c61634462c49158a04f2bf2a9a8e71f5d8937
Smulligan85 <sean.mulligan85@gmail.com>
2016-02-04 07:06:10 -0800

    Add SQL Table and Query methods

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES ("Mr. Chocolate", 42, "M", "Brown", "Hungry");
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES ("Rowdy", 2, "F", "Brown", "Happy");
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES ("Tabitha", 15, "M", "Black", "Angry");
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES ("Sergeant Brown", 12, "F", "Black", "Hungry");
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES ("Melissa", 100, "F", "White", "Happy");
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES ("Grinch", 9, "M", "Brown", "Hungry");
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES ("Wendy", 7, "F", "Brown", "Happy");
+INSERT INTO bears (name, age, gender, color, temperament)
+  VALUES (NULL, 12, "M", "Brown", "Hungry");
+
