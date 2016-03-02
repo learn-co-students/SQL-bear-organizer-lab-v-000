ec9659d24039260918b351eaf83413a1a2578bfa
Brian Holland <brian.holland@proatria.com>
2016-03-01 22:55:53 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, temperament, color) VALUES ('Mr Chocolate','sweet','brown');
+INSERT INTO bears (name, color) VALUES ('Rowdy', 'brown');
+INSERT INTO bears (name, age) VALUES ('Tabitha', 12);
+INSERT INTO bears (name) VALUES ('Sergeant Brown');
+INSERT INTO bears (name) VALUES ('Melissa');
+INSERT INTO bears (name) VALUES ('Grinch');
+INSERT INTO bears (name) VALUES ('Wendy');
+INSERT INTO bears (name) VALUES (NULL);
