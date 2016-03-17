72d7c90bdc40ba44cc31513a8bd99498573be889
Ann Lee <aplee29@users.noreply.github.com>
2016-03-17 08:39:03 -0400

    create new table, insert data, and write queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 35, "M", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 15, "M", "black", "energetic", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 21, "F", "black", "quiet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "white", "dopey", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "F", "brown", "mild", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 18, "M", "gray", "hyper", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 23, "F", "black", "silly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 32, "M", "brown", "sleepy", 0);
