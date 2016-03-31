6fc71d49a324b036246a64674435536508ba7624
viparthasarathy <viparthasarathy@gmail.com>
2016-03-31 09:57:33 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (2, "Mr. Chocolate", "male", "brown", "angry", "yes");
+
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (5, "Rowdy", "female", "brown", "goofy", "yes");
+
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (1, "Tabitha", "female", "black", "tired", "yes");
+
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (10, "Sergeant Brown", "male", "brown", "goofy", "yes");
+
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (14, "Melissa", "female", "brown", "angry", "no");
+
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (2, "Grinch", "male", "white", "bored", "yes");
+
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (3, "Wendy", "female", "brown", "goofy", "yes");
+
+INSERT INTO bears (age, name, gender, color, temperament, alive) VALUES (7, NULL, "male", "red", "murderous", "yes");
