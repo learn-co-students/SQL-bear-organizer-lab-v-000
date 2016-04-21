baa05b5074bbe901da85d993874fe5086a504f1d
Robert <robmcarrier@gmail.com>
2016-04-20 21:51:16 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender) VALUES ("Mr.Chocolate", 5, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Rowdy", 4, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Tabitha", 5, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Sergeant Brown", 5, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Melissa", 5, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Grinch", 5, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Wendy", 5, "F");
+INSERT INTO bears (name, age, gender) VALUES (NULL, 5, "M");
