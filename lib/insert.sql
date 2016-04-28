da55a43ff52649335147924cbd86038d428d35cb
miclp <michaelpeimani@gmail.com>
2016-04-28 15:13:17 -0700

    Add create.sql, insert.sql, and all SQL queries as methods in sql_queries.rb

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+--INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Chocolatey", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Brown", "Rambunctious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Brown", "MTV VJ from the 90's", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Militant", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "F", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "M", "Brown", "Shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "F", "Brown", "Helpful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 13, "M", "Brown", "Aggressive", 0);
