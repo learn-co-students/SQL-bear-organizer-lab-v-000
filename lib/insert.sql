7cdd45d118052cd23239c9345530999250570878
kirangp <kirangp@gmail.com>
2016-04-29 15:08:19 +1000

    Created table, inserted values

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate",
+   21, "M", "blue", "angry", 1);
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy",
+      22, "F", "green", "happy", 1);
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha",
+         23, "M", "yellow", "angry", 0);
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ("Sergent Brown",
+            24, "F", "blue", "cool", 1);
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ("Melissa",
+               25, "M", "orange", "angry", 1);
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ("Grinch",
+                  26, "F", "blue", "emotional", 0);
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ("Wendy",
+                     27, "M", "brown", "angry", 1);
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES (null,
+                        28, "F", "blue", "angry", 1);
