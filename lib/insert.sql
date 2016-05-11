5b819cf24f3d41121b0c4b8b179ae34d5111be8f
Michael Chen <michael.chen.1120@gmail.com>
2016-05-11 13:48:49 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+("Mr. Chocolate",NULL,"M",NULL,NULL,1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+("Rowdy",NULL,"M",NULL,"rowdy",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+("Tabitha",NULL,"F",NULL,NULL,1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+("Sergeant Brown",NULL,"M",NULL,NULL,1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+("Melissa",NULL,"F",NULL,NULL,1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+("Grinch",NULL,"M",NULL,"grumpy",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+("Wendy",NULL,"F",NULL,NULL,1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES
+(NULL,NULL,"M",NULL,"angry",1);
