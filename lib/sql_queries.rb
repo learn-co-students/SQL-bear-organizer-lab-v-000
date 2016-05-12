22200323c74b8b415f4c2ec78dc70b37dfad9e3e
jwolfe890 <jwolfe890@gmail.com>
2016-05-12 16:14:59 -0400

    all

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,29 +7,32 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT bears.name, bears.age FROM bears ORDER BY age DESC;"
+  "SELECT bears.name, bears.age FROM bears WHERE alive ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE MAX(age);"
+  "SELECT bears.name, bears.age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE MIN(age);" 
+  "SELECT bears.name, bears.age FROM bears ORDER BY age ASC LIMIT 1;" 
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) as color_count FROM bears GROUP BY color ORDER BY color_count DESC LIMIT 1;"
 end
 
+
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT TOTAL(bears) FROM bears WHERE temperament = goofy;"
+  "SELECT COUNT(name) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name IS NULL;"
 end
 
+
+ 
 
 
 
