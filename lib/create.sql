48682263cfc9952904e068e30a3cab616389f5ee
jwolfe890 <jwolfe890@gmail.com>
2016-05-11 20:15:51 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER, 
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
+
