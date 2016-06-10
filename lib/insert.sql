01aa18c8760ba07c7898c072aaab53d57f964069
bshap93 <benihana858@gmail.com>
2016-06-10 12:15:23 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES ("Mr. Chocolate", 12, "M", "Brown", "Lively", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES ("Rowdy", 3, "M", "Green", "Unruly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES ("Tabitha", 11, "F", "Green", "Introvert", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES ("Sergeant Brown", 6, "M", "Brown", "Orderly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES ("Melissa", 5, "F", "Grey", "Flirty", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES ("Grinch", 10, "M", "White", "Greedy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES ("Wendy", 4, "F", "Yellow", "Lively", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VAlUES (NULL, 4, "F", "Black", "Aggressive", 0);
