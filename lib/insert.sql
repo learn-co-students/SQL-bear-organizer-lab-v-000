481cba1ac7e95a3bed009cc16f4ff8a1f96609dd
Shoko Furushita <shktkt219@gmail.com>
2016-06-10 17:55:04 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "M", "Brown", "sweet", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 15, "M", "white", "anger", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 30, "F", "pink", "modest", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 2, "F", "Brown", "calm", 1);
+INSERT INTO bears VALUES (5, "Melissa", 17, "F", "orange", "plain", 0);
+INSERT INTO bears VALUES (6, "Grinch", 89, "M", "gold", "wise", 0);
+INSERT INTO bears VALUES (7, "Wendy", 27, "F", "green", "sweet", 1);
+INSERT INTO bears VALUES (8, NULL, 3, "M", "Brown", "aggressive", 0);
