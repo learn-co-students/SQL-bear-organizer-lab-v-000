b87f36da6f6182e43955644ae6ead9ef05317dc4
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-15 16:34:17 +0000

    After passing most tests

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "brown", "cuddly", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "brown", "cute", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "brown", "cuddly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "brown", "cuddly", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "white", "gruff", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "brown", "gruff", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "M", "red", "wily", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "brown", "gruff", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "M", "red", "goofy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "white", "cuddly", 1);
 INSERT INTO bears (age, gender, color, temperament, alive) VALUES (3, "F", "black", "protective", 1);
