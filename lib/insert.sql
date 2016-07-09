f0af64e032fe700336ad74fc06230ec37494c5e3
Sai Petluri <spetluri.1@gmail.com>
2016-07-09 19:52:40 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name,age,gender,color,temperament,alive) values ('Mr. Chocolate',12,'M','Black',"Happy",0);
+insert into bears (name,age,gender,color,temperament,alive) values ('Rowdy',13,'F','Brown',"Sad",1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Tabitha',14,'M','Blue',"Angry",0);
+insert into bears (name,age,gender,color,temperament,alive) values ('Sergeant Brown',15,'F',"Indigo",'Happy',1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Melissa',16,'M','Violet',"Sad",0);
+insert into bears (name,age,gender,color,temperament,alive) values ('Grinch',17,'F','Red',"Angry",1);
+insert into bears (name,age,gender,color,temperament,alive) values ('Wendy',18,'M','Orange',"Happy",0);
+insert into bears (name,age,gender,color,temperament,alive) values (NULL,19,'F','Yellow',"Sad",1);
