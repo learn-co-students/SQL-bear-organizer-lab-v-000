5855b5db12a8db1ec57f62efd600834231c7d9a7
TRIP <trip@tripscripts.net>
2016-07-29 08:47:49 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears values (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+insert into bears values (2,"Rowdy", 10, "M", "black", "intense", 1);
+insert into bears values (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+insert into bears values (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+insert into bears values (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+insert into bears values (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+insert into bears values (7,"Wendy", 12, "F", "Blue", "naive", 0);
+insert into bears values (8,null, 20, "M", "black", "aggressive", 0);
