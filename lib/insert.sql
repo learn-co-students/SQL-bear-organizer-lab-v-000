5b360daec9a3805dacf39361e3bba5ac7998e292
Perla Yates <pianistps@yahoo.com>
2016-08-18 21:12:21 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 1, 'M', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 2, 'M', 'white', 'sad', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 1, 'F', 'brown', 'hungry', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 5, 'M', 'brown', 'lively', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 6, 'F', 'white', 'sad', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 8, 'M', 'black', 'mad', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 3, 'F', 'black', 'happy', 1);
+INSERT INTO bears VALUES (8, NULL, 2, 'M', 'brown', 'happy', 0);
