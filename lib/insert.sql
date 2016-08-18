11224d9b43f3773211db40f6cb475c0272b2310a
alice <github email address>
2016-08-18 19:57:03 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 25, "F", "Black", "Joyful", 0);
+INSERT INTO bears VALUES (2,"Rowdy", 16, "M", "Brown", "Peaceful", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 52, "F", "Brown", "Lazy", 0);
+INSERT INTO bears VALUES (4,"Sergeant Black", 40, "F", "Brown", "Sleepy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 61, "M", "Black", "Peaceful", 0);
+INSERT INTO bears VALUES (6,"Grinch", 85, "F", "Brown", "Peaceful", 1);
+INSERT INTO bears VALUES (7,"Wendy", 2, "F", "Black", "Lazy", 0);
+INSERT INTO bears VALUES (8,null, 10, "M", "Brown", "Angry", 1);
