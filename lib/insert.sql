c637c80f9dde6267e25be1fa155b45f12235302c
David Kennell <davidkennell0@gmail.com>
2016-09-10 17:42:14 +0000

    Passed the lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 11, "Male", "Brown", "Psychotic", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 12, "Male", "Green", "Psychic", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 13, "Female", "Polka Dots", "Skeptical", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 10, "Male", "Zebra Stripes", "Conformist", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 15, "Female", "Roan", "Happy", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 4, "Male", "White", "Polar", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 6, "Female", "Brownish", "Determined", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 11, "Male", "Black", "Ghostlike", "true");
