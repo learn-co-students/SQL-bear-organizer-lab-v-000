750390c12be7d243442987f3c20d8246a2750353
rgylling <mikegylling@yahoo.com>
2016-09-16 19:09:35 -0700

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears(
+name TEXT,
+age INTEGER,
+gender TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER,
+id INTEGER PRIMARY KEY);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Mr. Chocolate', 1, 'male', 'blue', 'mean','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Rowdy', 2, 'male', 'red', 'happy','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Tabitha', 3, 'female', 'brown', 'dopey','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Sergeant Brown', 4, 'male', 'black', 'sleepy','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Melissa', 5, 'female', 'green', 'psychotic','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Grinch', 6, 'male', 'red', 'mean','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES('Wendy', 6, 'female', 'red', 'mean','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES(NULL, 7, 'male', 'blue', 'happy','true');
