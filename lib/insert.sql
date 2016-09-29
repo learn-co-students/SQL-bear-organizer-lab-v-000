cba26f46c348230f779a1289d1d75dfd0f22419c
John Verdone <jmverdone@gmail.com>
2016-09-28 22:20:39 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 3, "M", "Brown", "Mean", 0),
+('Rowdy', 3, "M", "Brown", "Mean", 1),
+('Tabitha', 3, "M", "Brown", "Mean", 0),
+('Sergeant Brown', 3, "M", "Brown", "Mean", 1),
+('Melissa', 3, "M", "Brown", "Mean", 0),
+('Grinch', 3, "M", "Brown", "Mean", 0),
+('Wendy', 3, "M", "Brown", "Mean", 1);
+
+
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES
+(3, "M", "Brown", "Mean", 1);
