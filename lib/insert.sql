9dc9d9c0ceca99bc8315edf950c69839dba6710a
Clare <clare.e.horton@gmail.com>
2016-09-30 05:34:51 +0000

    create and insert done, 2/8 on select

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 18, "F", "brown", "feisty", 1),
+(2, "Rowdy", 6, "M", "black", "withdrawn", 0),
+(3, "Tabitha", 5, "F", "grizzly", "flirty", 0),
+(4, "Sergeant Brown", 2, "F", "black", "militaristic", 1),
+(5, "Melissa", 9, "F", "polar", "cheerleaderesque", 0),
+(6, "Grinch", 10, "M", "green", "grumpy", 1),
+(7, "Wendy", 7, "F", "polar", "flighty", 1),
+(8, NULL, 4, "M", "grizzly", "combative", 0);
