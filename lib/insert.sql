0721c3f673d551c2c201758f79421c60c7c6ac2a
mjdele <mike.delehant@gmail.com>
2016-10-27 17:57:41 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("RZA", 25, "M", "brown", "zen", 0),
+("Ghostface Killah", 35, "M", "black", "intense", 1),
+("Method Man", 15, "M", "brown", "Nice", 1),
+("Ol' Dirty Bastard", 39, "M", "auburn", "Unpredictable", 0),
+("Raekwon", 27, "M", "dark brown", "goofy", 1),
+("GZA", 46, "M", "Black", "Strange", 1),
+("U-God", 22, "M", "Blue", "naive", 1),
+(null, 25, "M", "black", "playful", 0);
