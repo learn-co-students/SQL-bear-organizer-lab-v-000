0884cef3cff007463a65a8e31ae8eb1bc49d58fd
Ray Mulligan <rmullig2@optonline.net>
2016-11-05 02:26:28 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name, age, gender, color, temperament, alive) values ('Mr. Chocolate', 12, 'black', 'M', 'smooth', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Rowdy', 20, 'black', 'M', 'aggressive', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Tabitha', 2, 'grey', 'F', 'bewitching', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown', 5, 'brown', 'M', 'goofy', 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Melissa', 18, 'white', 'F', 'wild', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 14, 'grey', 'M', 'grumpy', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Wendy', 6, 'brown', 'F', 'goofy', 1);
+insert into bears (name, age, gender, color, temperament, alive) values (null, 1, 'white', 'F', 'playful', 1);
