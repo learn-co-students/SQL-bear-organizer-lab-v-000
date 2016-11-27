67783638a84bfd95d6ba8297b0a28a321adc513c
Pia Kim <pkim2320@gmail.com>
2016-11-27 04:50:06 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 10, "M", "brown", "sweet", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 15, "M", "white", "mean", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 44, "F", "gray", "calm", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 50, "M", "brown", "angry", 1);
+INSERT INTO bears VALUES (5, "Melissa", 4, "F", "black", "nice", 1);
+INSERT INTO bears VALUES (6, "Grinch", 20, "M", "green", "mean", 1);
+INSERT INTO bears VALUES (7, "Wendy", 16, "F", "pink", "happy", 1);
+INSERT INTO bears VALUES (8, null, 30, "M", "brown", "sad", 1);
