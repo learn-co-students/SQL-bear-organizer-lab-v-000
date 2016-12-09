33613a0eb182c6ba1463b6d6743f3f4543f5c64d
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-12-09 20:46:17 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE bears(id INTEGER PRIMARY KEY,
+                  name text,
+                  age INTEGER,
+                  gender char(1),
+                  color TEXT,
+                  temperament TEXT,
+                  alive boolean);
