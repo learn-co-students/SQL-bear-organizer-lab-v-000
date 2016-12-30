d4d402e2d98b5b79ac799d4b20ccee62c375f23f
Benny Hudson Hung <wakenightryu@gmail.com>
2016-12-30 05:09:54 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 15, "M", "brown", "laid-back", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 30, "F", "black", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "pink", "sweet", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 42, "M", "brown", "militant", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 26, "F", "red", "sarcastic", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 67, "M", "green", "pessimistic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 11, "F", "white", "romantic", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 43, "M", "blue", "stupid", 0);
