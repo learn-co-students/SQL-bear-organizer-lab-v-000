c1180b20895a25451c6a0d1cfa086f7e3148e005
Sarah Rosenshine <srosenshine@gmail.com>
2016-12-31 23:06:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'F', 'black', 'kind', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, 'F', 'black', 'kind', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'black', 'kind', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sargeant Brown', 7, 'F', 'black', 'kind', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'black', 'kind', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'F', 'black', 'kind', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'black', 'kind', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 7, 'F', 'black', 'kind', 1);
--- lib/seed.sql
+++ lib/seed.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
 (2,"Rowdy", 10, "M", "black", "intense", 1),
