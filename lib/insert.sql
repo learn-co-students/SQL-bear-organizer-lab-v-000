611606243421515c1ad5cf90c7ca06f9c3d8bce0
Joseph Bannerman <bannermanj11@yahoo.com>
2016-12-31 01:03:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'F', 'brown', 'nice', '1');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'black', 'aggressive', '1');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 9, 'M', 'brown', 'friendly', '1');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'F', 'white', 'friendly', '1');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'M', 'brown', 'nice', '0');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'black', 'goofy', '1');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 4, 'M', 'white', 'nice', '1');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 5, 'M', 'brown', 'nice', '0');
