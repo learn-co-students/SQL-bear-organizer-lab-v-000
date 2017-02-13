f9bfd9488a1308be06ff0b08ce0747c68878037d
Alex Glover <alexanderbglover@gmail.com>
2017-02-13 16:35:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 15, "M", "brown", "friendly", "true");
+  insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 12, "M", "brown", "rowdy", "true");
+    insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 3, "F", "black", "peaceful", "true");
+      insert into bears (name, age, gender, color, temperament, alive) values ("Sergeant Brown", 19, "M", "brown", "militant", "true");
+        insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 8, "F", "brown", "stanky", "true");
+          insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 13, "M", "green", "grumpy", "true");
+            insert into bears (name, age, gender, color, temperament, alive) values ("Wendy", 20, "F", "black", "forgetful", "true");
+              insert into bears (age, gender, color, temperament, alive) values (15, "M", "black", "deadly", "true");
