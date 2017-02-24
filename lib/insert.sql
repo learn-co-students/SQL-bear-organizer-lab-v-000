ad09fc73a94a8bb1cdf82398df7e563e5094b908
Juan Gongora <gongora.animations@gmail.com>
2017-02-24 18:53:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender) VALUES
+("Mr. Chocolate", 12, "M"),
+("Rowdy", 7, "M"),
+("Tabitha", 12, "F"),
+("Sergeant Brown", 30, "M"),
+("Melissa", 21, "F"),
+("Grinch", 48, "M"),
+("Wendy", 9, "F"),
+(NULL, 28, "M");
