d869edba0d05b4b8b2afc3c610cc3529640e4bf3
bentonwong <bentonwong@gmail.com>
2017-03-05 19:59:15 -0800

    fixed table

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 9, 'M', 'Brown', 'Crazy', TRUE)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'Red', 'Crazy', FALSE)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'Pink', 'Insane', FALSE)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'Brown', 'Happy', FALSE)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 85, 'F', 'Pink', 'Sad', TRUE)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 100, 'M', 'Brown', 'Sad', FALSE)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'Pink', 'Happy', TRUE)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, 'F', 'Pink', 'Happy', FALSE)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 9, 'M', 'Brown', 'Crazy', 'TRUE');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'Red', 'Crazy', 'FALSE');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'Pink', 'Insane', 'FALSE');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'Brown', 'Happy', 'FALSE');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 85, 'F', 'Pink', 'Sad', 'TRUE');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 100, 'M', 'Brown', 'Sad', 'FALSE');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'Pink', 'Happy', 'TRUE');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, 'F', 'Pink', 'Happy', 'FALSE');
