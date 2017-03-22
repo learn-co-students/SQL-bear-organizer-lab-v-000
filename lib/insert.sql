a1736fd5edb3fc219a8932e5b26c4a45c9024bff
Erika Hughes <ehughes@knights.ucf.edu>
2017-03-22 16:51:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 13, "M", "dark brown", "chill", 1),
+("Rowdy", 9, "M", "black", "rowdy", 1),
+("Tabitha", 16, "F", "light brown", "curious", 1),
+("Sergeant Brown", 20, "F", "dark brown", "stern", 1),
+("Melissa", 7, "F", "black", "silly", 1),
+("Grinch", 17, "M", "dark brown", "mean", 1),
+("Wendy", 12, "F", "light brown", "skittish", 1),
+(NULL, 11, "M", "dark brown", "territorial", 1);
