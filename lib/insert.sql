008af3a75a5ee23d87a86d824672b432312fa7cd
mxdavis <md01@me.com>
2017-03-24 00:21:22 +0200

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", 3, "M", "Brown", "calm", "yes");
+    VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Rowdy", 1, "M", "Brown", "rowdy", "yes");
+    VALUES ("Rowdy", 10, "M", "black", "intense", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Tabitha", 3, "F", "Black", "relaxed", "yes");
+    VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Sergeant Brown", 4, "M", "Brown", "angry", "yes");
+    VALUES ("Sargeant Brown", 19, "M", "Green", "Slimy", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Melissa", 5, "F", "White", "calm", "yes");
+    VALUES ("Melissa", 13, "F", "dark brown", "goofy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Grinch", 3, "M", "Brown", "angry", "yes");
+    VALUES ("Grinch", 2, "M", "Black", "Grinchy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Wendy", 3, "M", "Black", "rowdy", "yes");
+    VALUES ("Wendy", 6, "F", "Blue", "naive", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES (NULL, 7, "M", "Brown", "angry", "yes");
+    VALUES (NULL, 20, "M", "black", "aggressive", 0);
