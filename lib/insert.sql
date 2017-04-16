f3d911698bd586011916b5035568177fd22b9e65
ETphonehomeslice <mk.etlinger@gmail.com>
2017-04-16 08:59:48 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "pink", "coo", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "M", "pink", "coo", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "M", "pink", "coo", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "F", "pink", "coo", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 7, "M", "pink", "coo", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "F", "pink", "coo", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 16, "M", "pink", "coo", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 300, "M", "opaque", "killer", 0);
