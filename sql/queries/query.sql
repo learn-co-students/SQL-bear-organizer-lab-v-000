01f80cb3ddff0571dc92c3b0c570faa57912b51b
William Barela <william.barela.dev@gmail.com>
2017-04-30 13:00:03 +0900

    added sql files from local

---
+++ sql/queries/query.sql
@@ -0,0 +1,13 @@
+.header on
+.mode column
+.width auto
+
+SELECT * FROM bears;
+SELECT name, age FROM bears WHERE gender = "F";
+SELECT name FROM bears ORDER BY name ASC;
+SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;
+SELECT name, age FROM bears ORDER BY age DESC LIMIT 1; 
+SELECT name, age FROM bears ORDER BY age ASC LIMIT 1; 
+SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1; 
+SELECT COUNT(temperament) FROM bears WHERE temperament = "goofy";
+SELECT * FROM bears WHERE name IS NULL;
