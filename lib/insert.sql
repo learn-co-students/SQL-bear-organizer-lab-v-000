a5e427f62f6091ebbe6c12509247fe76838d8cbd
Will-Sauer <will@heueso.com>
2017-06-01 08:42:53 -0700

    insert spec passing

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(1,"Mr. Chocolate", 12, "M", "black", "happy", 0),
+(2, "Rowdy", 4, "F", "brown", "mean", 1),
+(3, "Tabitha", 9, "M", "golden", "happy", 1),
+(4, "Sergeant Brown", 7, "M", "black", "mean", 1),
+(5, "Melissa", 3, "F", "golden", "mean", 1),
+(6, "Grinch", 13, "M", "black", "mean", 1),
+(7, "Wendy", 8, "F", "brown", "happy", 1),
+(8, null, 10, "M", "black", "mean", 0);
+
