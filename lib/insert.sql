7b7e3d7be8601bb5933990e2667b7033729e6c63
Victoria Sowell <tpms123@gmail.com>
2017-06-06 20:33:05 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 10, "M", "Brown", "calm", 0),
+(2, "Rowdy", 20, "M", "Black", "Aggressive", 1),
+(3, "Tabitha", 13, "F", "Brown", "Nice", 0),
+(4, "Sergeant Brown", 17, "M", "Black", "Nice", 1),
+(5, "Melissa", 5, "F", "Brown", "Calm", 0),
+(6, "Grinch", 8, "M", "Blue", "Aggressive", 0),
+(7, "Wendy", 19, "F", "Black", "Mean", 1),
+(8, null, 20, "M", "black", "aggressive", 0);
