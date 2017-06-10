f839e452871b9eec99563bd9e847e8ac029a7095
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-06-10 02:17:22 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+  VALUES("Mr. Chocolate", 8, "M", "black", "cool", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+  VALUES("Rowdy", 12, "M", "black", "crazy", 0);
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+  VALUES("Tabitha", 2, "F", "brown", "COOL", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+  VALUES("Sergeant Brown", 21, "M", "white", "wild", 0);
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+  VALUES("Melissa", 12, "F", "brown", "happy", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+  VALUES("Grinch", 15, "F", "brown", "intense", 0);
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+  VALUES("Wendy", 12, "M", "white", "wow", 1);
+INSERT INTO bears(name,age,gender,color,temperament,alive)
+    VALUES(NULL, 14, "F", "black", "angry", 1);
