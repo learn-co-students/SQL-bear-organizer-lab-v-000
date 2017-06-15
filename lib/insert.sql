952882495125615d0f196b495ad7e4900127c9e8
Victoria Sowell <tpms123@gmail.com>
2017-06-15 00:12:40 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,16 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1, "Mr. Chocolate", 10, "M", "Brown", "calm", 0),
-(2, "Rowdy", 20, "M", "Black", "Aggressive", 1),
-(3, "Tabitha", 13, "F", "Brown", "Nice", 0),
-(4, "Sergeant Brown", 17, "M", "Black", "Nice", 1),
-(5, "Melissa", 5, "F", "Brown", "Calm", 0),
-(6, "Grinch", 8, "M", "Blue", "Aggressive", 0),
-(7, "Wendy", 19, "F", "Black", "Mean", 1),
-(8, null, 20, "M", "black", "aggressive", 0);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+( "Mr. Chocolate", 10, "M", "Brown", "calm", 0);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+("Rowdy", 20, "M", "Black", "Aggressive", 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+( "Tabitha", 13, "F", "Brown", "Nice", 0);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+( "Sergeant Brown", 17, "M", "Black", "Nice", 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+( "Melissa", 5, "F", "Brown", "Calm", 0);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+( "Grinch", 8, "M", "Blue", "Aggressive", 0);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+( "Wendy", 19, "F", "Black", "Mean", 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES
+(null, 20, "M", "black", "aggressive", 0);
