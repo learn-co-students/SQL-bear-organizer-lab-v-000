015bef2b076c6aee766827027f6e9cb4cfb2cc0c
Richard Burd <richardburdx@yahoo.com>
2017-06-26 20:40:00 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 35, "M", "Martruse", "douchebag", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 65, "M", "Martruse", "douchebag", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 12, "F", "Martruse", "docile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, "F", "Martruse", "nimble", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 120, "M", "Martruse", "nervious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 87, "M", "Martruse", "OCD", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 52, "F", "Martruse", "bi-polar", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 52, "F", "Martruse", "bi-polar", 1);
