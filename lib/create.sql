5e3c15e4eeef083b5678c048aed20f1bdaa985a7
Minh Nguyen <minhpcd@gmail.com>
2017-07-02 04:30:43 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears(
+  ID INTEGER PRIMARY KEY,
+     name TEXT,
+     age INTEGER,
+     gender TEXT,
+       -- m or f
+     color TEXT,
+     temperament TEXT,
+     alive INTEGER
+       -- 0(false) and 1 (true)
+);
