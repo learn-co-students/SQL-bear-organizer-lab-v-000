bd398c6fb20e6c39a9bfb94a6cd708d362c544cd
Jennifer Haddock <jentx123@hotmail.com>
2017-07-08 04:52:08 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,135 @@
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'Mr. Chocolate',
+  12,
+  'M',
+  'Brown',
+  'Chill',
+  'Y'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'Rowdy',
+  2,
+  'M',
+  'Black',
+  'R-O-W-D-I-E',
+  'Y'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'Tabitha',
+  '30',
+  'F',
+  'Black',
+  'Old',
+  'Y'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'Sergeant Brown',
+  13,
+  'M',
+  'Brown',
+  'Mean',
+  'Y'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'Melissa',
+  10,
+  'F',
+  'Brown',
+  'Nice',
+  'Y'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'Grinch',
+  40,
+  'M',
+  'Black',
+  'Super mean',
+  'Y'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'Wendy',
+  5,
+  'F',
+  'Black',
+  'Protecting her cubs',
+  'Y'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  NULL,
+  15,
+  'M',
+  'Brown',
+  'Angry and hungry',
+  'Y'
+);
