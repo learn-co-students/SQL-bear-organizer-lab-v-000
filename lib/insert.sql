5d834e39542faf405551901727f95a5055566262
Elvin <Dimix181@Gmail.com>
2017-07-25 22:10:49 -0400

    all test are passing with the exception of part 3

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(id,name,age,color,temperament,gender,alive) values(1,"Mr. Chocolate",2,"brown","happy","Female",1);
+
+INSERT INTO bears(name,age,color,temperament,gender,alive) values("Rowdy",2,"brown","happy","Female",1);
+
+INSERT INTO bears(name,age,color,temperament,gender,alive) values("Tabitha",2,"brown","happy","Female",1);
+
+INSERT INTO bears(name,age,color,temperament,gender,alive) values("Sergeant Brown",2,"brown","happy","Female",1);
+
+INSERT INTO bears(name,age,color,temperament,gender,alive) values("Melissa",2,"brown","happy","Female",1);
+
+INSERT INTO bears(name,age,color,temperament,gender,alive) values("Grinch",2,"brown","happy","Female",1);
+
+INSERT INTO bears(name,age,color,temperament,gender,alive) values("Wendy",2,"brown","happy","Female",1);
+
+INSERT INTO bears(age,color,temperament,gender,alive) values(2,"White","Angry","Male",1);
