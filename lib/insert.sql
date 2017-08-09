0da24fcb85f27a8b054444b4f49734f030432cdd
Dominique McCloud <niquelejon@gmail.com>
2017-08-09 19:18:34 +0000

    completed exercise

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament) VALUES("Mr. Chocolate",11,"M","blue","nice");
+INSERT INTO bears(name, age, gender, color, temperament) VALUES("Rowdy",11,"M","red","rough");
+INSERT INTO bears(name, age, gender, color, temperament) VALUES("Tabitha",9,"F","yellow","mellow");
+INSERT INTO bears(name, age, gender, color, temperament) VALUES("Sergeant Brown",10,"M","brown","serious");
+INSERT INTO bears(name, age, gender, color, temperament) VALUES("Melissa",15,"F","lavendar","kind");
+INSERT INTO bears(name, age, gender, color, temperament) VALUES("Grinch",12,"M","green","hyper");
+INSERT INTO bears(name, age, gender, color, temperament) VALUES("Wendy",15,"F","orange","bubbly");
+INSERT INTO bears(name, age, gender, color, temperament) VALUES(NULL,13,"F","silver","mysterious");
