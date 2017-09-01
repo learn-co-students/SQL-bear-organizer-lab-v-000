8071eeaaf62fcbfda521a09d5bee1ee0723a6f36
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-09-01 00:04:14 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES ('Mr. Chocolate',2, "M","goofy",0);
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES ('Rowdy',1, "M","sweet",1);
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES ('Tabitha',3, "F","goofy",0);
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES ('Sergeant Brown',4,"M","goofy",1);
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES ('Melissa',5, "F","Sweet",0);
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES ('Grinch',3, "M","goofy",1);
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES ('Wendy',2, "F", "Sweet",0);
+INSERT INTO bears(name,age,gender,temperament,alive) VALUES (NULL,1, "F","goofy",1);
