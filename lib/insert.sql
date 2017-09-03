1b7f425ef99790c2720cc96f5f5a72c6407bc88e
ryanwevans <ryanwevans@icloud.com>
2017-09-03 15:57:48 -0700

    added SQL code to create.sql, insert.sql and sql_queries.rb

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 18, "M", "Chocolate", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 15, "M", "Red", "Wild", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "F", "orange", "Playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 25, "M", "brown", "Bossy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "F", "White", "Nurturing", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 32, "M", "Green", "Grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "F", "blue", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, "M", "red", "Agressive", 0);
