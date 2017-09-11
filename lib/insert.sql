44360eb5054eb67daddafbfd818b268cf7c9a5c5
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-09-11 15:57:38 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Mr. Chocolate",7,"M","Dark Brown","friendly",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Rowdy",2,"F","Brown","playful",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Tabitha",25,"F","Orange","sleepy",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Sergeant Brown",15,"M","Light Brown","angry",0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Melissa",8,"F","Blonde","fierce",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Grinch",19,"M","Brown","grinchy",0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Wendy",13,"F","Burnt Orange","playful",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES(null,14,"F","Brown","hostile",0);
