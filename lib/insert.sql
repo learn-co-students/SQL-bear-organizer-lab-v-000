596ff5ead59c0df01a74951bcb4a0fdffb553785
Jameson Bass <jamesonbass@gmail.com>
2017-09-19 19:24:00 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (1, 'Mr. Chocolate', 7, 'Male', 'Brown', 'Aggressive', 'true');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (2, 'Rowdy', 13, 'Female', 'Black', 'Docile', 'true');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (3, 'Tabitha', 4, 'Male', 'Brown', 'Aggressive', 'false');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (4, 'Sergeant Brown', 2, 'Male', 'Black', "Docile", 'true');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (5, 'Melissa', 3, 'Female', 'Black', 'Docile', 'false');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (6, 'Grinch', 6, 'Male', 'Brown', 'Aggressive', 'false');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (7, 'Wendy', 8, 'Female', 'Brown', 'Aggressive', 'true');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (8, NULL,  10, 'Female', 'Black', 'Docile', 'true');
