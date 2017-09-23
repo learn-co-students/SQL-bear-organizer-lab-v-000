b763a7bc9a765af77399fc5af7ddf7a9d8284df2
Sophia H <shhubeny@gmail.com>
2017-09-23 02:36:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "F", "brown", "moody", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "white","calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "F", "black","energetic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "brown", "sleepy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 40, "F", "brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 14, "M", "brown", "cranky", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "F", "brown", "hyper", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null, 4, "M", "black", "murdery", 1);
--- spec/insert_spec.rb
+++ spec/insert_spec.rb
@@ -5,11 +5,11 @@ describe 'populating the bears table' do
     @sql_runner.execute_create_file
     @sql_runner.execute_insert_file
   end
-
+ 
   it 'has 8 bears' do
     expect(@db.execute("SELECT COUNT(*) FROM bears;").flatten[0]).to eq(8)
   end
-  
+
   it 'has an unnamed bear' do
     expect(@db.execute("SELECT COUNT(*) FROM bears WHERE name IS NULL;").flatten[0]).to eq(1)
   end
