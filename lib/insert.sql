a12e2a22a9a05f1a38124522091584b774b230c1
brandon wirz <brandonwirz2@brandons-MacBook-Pro.local>
2017-10-02 17:08:00 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "tan", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 17, "M", "brownish", "pissy", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 12, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 8, "M", "dark black", "docile", 1);
+INSERT INTO bears VALUES(5,"Melissa", 7, "F", "tan", "fast", 1);
+INSERT INTO bears VALUES (6,"Grinch", 8, "M", "dark brown", "stinky", 1);
+INSERT INTO bears VALUES (7,"Wendy", 3, "F", "black", "lazy", 0);
+INSERT INTO bears VALUES (8,null, 3, "F", "black", "aggressive", 1);
