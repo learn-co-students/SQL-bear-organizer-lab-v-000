b65ae8a58ecccc1893f5efdc6f83b70b294290c1
Jon Hinson <jonhinsonfx@gmail.com>
2017-11-17 00:13:13 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color) VALUES ("Mr. Chocolate", 3 , "M", "brown");
+INSERT INTO bears (name, age, gender, color) VALUES ("Rowdy", 4 , "M", "black");
+INSERT INTO bears (name, age, gender, color) VALUES ("Tabitha", 2 , "F", "grey");
+INSERT INTO bears (name, age, gender, color) VALUES ("Sergeant Brown", 4 , "M", "brown");
+INSERT INTO bears (name, age, gender, color) VALUES ("Melissa", 3 , "F", "grey");
+INSERT INTO bears (name, age, gender, color) VALUES ("Grinch", 2 , "M", "black");
+INSERT INTO bears (name, age, gender, color) VALUES ("Wendy", 2 , "F", "grey");
+INSERT INTO bears (name, age, gender, color) VALUES (NULL, 2 , "M", "brown");
