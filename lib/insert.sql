b2e0688b199fcb81f92dc9ded47c053c15144492
Amanda Ackerman <ackerm44@gmail.com>
2017-11-27 15:55:10 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr.Chocolate", 4, "M", "dark brown", "friendly", 1),
+(2, "Rowdy", 8, "M", "spotted", "hyper", 1),
+(3, "Tabitha", 2, "F", "light brown", "calm", 1),
+(4, "Sergeant Brown", 11, "M", "very brown", "authoritative", 1),
+(5, "Melissa", 5, "M", "velvet brown", "stoic", 1),
+(6, "Grinch", 9, "M", "brown", "moody", 1),
+(7, "Wendy", 1, "F", "striped", "trusting", 1),
+(8, null, 15, "M", "patchy brown", "agressive", 0)
