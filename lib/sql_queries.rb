68427c8e7992ccccf975bfa081a6f39a4dcb6a64
github username <github email address>
2017-12-06 16:56:38 -0600

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT color FROM bears WHERE color = MAX(Cnt);"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT id, name, age, gender, color, temperament, alive FROM bears WHERE id = 8;"
 end
