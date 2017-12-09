5ba59609f707aae5a34d411e0a6381c218409415
Matthew Hunger <matt.hunger@gmail.com>
2017-12-09 12:16:41 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 20, "M", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 20, "M", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 20, "M", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 20, "M", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 20, "M", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 20, "M", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 20, "M", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 20, "M", "brown", "curious", 1);
