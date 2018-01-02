85b98b6f5d0f5a6f2651225a341b3067a40c273a
Blakeley Bond <burrick2003@yahoo.com>
2018-01-02 21:06:36 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 1, "M", "brown", "playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 2, "F", "dark brown", "risk-averse", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 3, "M", "light brown", "very maternal", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Seargent Brown", 4, "F", "dirty brown", "charming", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 5, "M", "filthy brown"), "rather pleasant", 1;
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 5, "F", "dingy brown", "shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 7, "F", "dodgy brown", "bashful", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (null, 9, "M", "decrepit brown", "a little nasty", 1);
