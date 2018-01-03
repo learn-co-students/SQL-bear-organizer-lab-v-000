9d980d352c11e8656a542fd959483308c4223d7e
Blakeley Bond <burrick2003@yahoo.com>
2018-01-03 04:30:11 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE bears
     (id INTEGER PRIMARY KEY,
     name TEXT,
-    age, INTEGER,
-    gender, TEXT,
-    color, TEXT,
-    temperament, TEXT,
-    alive, INTEGER);
+    age INTEGER,
+    gender CHAR(1),
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN);
