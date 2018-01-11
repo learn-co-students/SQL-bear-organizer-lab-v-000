f9f3c07b22cdbcd51fd5a19181820f62bb086c1b
Charity <Youngblood.charity@gmail.com>
2018-01-10 17:39:23 -0800

    added table values in insert.sql

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'F', 'Brown', 'Crazy' , 1); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'Brown', 'Calm', 0); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Brown', 'Crazy', 0); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M','Brown', 'Crazy', 0); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 17, 'F','Brown', 'Calm', 0); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 34, 'M', 'Black', 'Crazy', 0); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'Black', 'Crazy', 1); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'F', 'Black', 'Crazy', 1); 
