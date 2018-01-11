449aba91d7ac71fdbc44dd300321acb093592549
charityyoungblood <youngblood.charity@gmail.com>
2018-01-10 18:13:43 -0800

    added query for oldest bear in sql_queries.rb

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'F', 'Brown', 'Crazy' , 1); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'Brown', 'Calm', 0); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Brown', 'Crazy', 0); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M','Brown', 'Crazy', 0); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 17, 'F','Brown', 'Calm', 0); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 34, 'M', 'Black', 'Crazy', 0); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'Black', 'Crazy', 1); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'F', 'Black', 'Crazy', 1); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+("Rowdy", 10, "M", "black", "intense", 1),
+("Tabitha", 6, "F", "dark brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+("Melissa", 13, "F", "dark brown", "goofy", 1),
+("Grinch", 2, "M", "Black", "Grinchy", 1),
+("Wendy", 6, "F", "Blue", "naive", 1),
+(null, 20, "M", "black", "aggressive", 0);
