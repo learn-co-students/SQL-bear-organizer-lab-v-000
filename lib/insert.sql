4afbb5010932dad1c00b795abad1910841605a24
Charity <Youngblood.charity@gmail.com>
2018-01-10 17:25:38 -0800

    added 8 placeholders for bears in insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (); 
