929a2f82b1f69654dfc85d1d4629fc201282b82a
Rygel-XVI <micha232@umn.edu>
2018-01-16 13:25:25 -0800

    finished create and insert files

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, 1, "Brown", "Cuddly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, 1, "Roan", "Energetic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, 0, "Yellow", "Shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 0, 8, "Black", "Strict", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, 0, "Grey", "Rowdy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 15, 1, "Grey", "Grumpy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, 0, "White", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 0, 0, "Yellow", "Quiet", 1);
