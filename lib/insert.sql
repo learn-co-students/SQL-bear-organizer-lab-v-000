de983faab24ee93ea3e01d447bf765bb5f6b643a
Karen Berba <kberba@gmail.com>
2018-02-02 04:26:16 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 10, "M", " dark brown", "calm", 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 10, "M", "dark brown", "restless", 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 7, "F", "blue", "angry", 0);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 55, "M", "brown", "strict", 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 4, "F", "dark brown", "playful", 0);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 23, "M", "black with a white splotch", "distrusting", 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 111, "F", "sandy tan", "quiet", 0);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 1000, "M", "mustard yellow", "angry", 1);
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
