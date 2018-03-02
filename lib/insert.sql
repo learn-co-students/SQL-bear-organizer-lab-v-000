215d96db9dbdb2512b3300678f8f59e7ffc3c52b
Joma Ejercito <jomaejercito@gmail.com>
2018-03-02 06:17:52 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 10, "M", "Dark Brown", "Gentle", 1),
+(2,"Rowdy", 5, "M", "Red", "Rash", 1),
+(3,"Tabitha", 9, "F", "Purple", "Timid", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Brave", 0),
+(5,"Melissa", 7, "F", "Pink", "Jolly", 1),
+(6,"Grinch", 19, "M", "Green", "Serious", 0),
+(7,"Wendy", 9, "F", "Yellow", "Sassy", 1),
+(8,null, 20, "M", "White", "Rash", 0);
