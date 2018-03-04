8c05d1b00e2be0cab8ff34053d79d2fc2ec0f8ad
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-04 20:50:11 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate",5,"M","Brown","Calm",1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy",6,"M","Red-Brown","Goofy",1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha",10,"F","Brown","Calm",1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",16,"M","Dark Brown","Aggressive",0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa",8,"F","Red-Brown","Goofy",0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch",7,"M","Brown","Grumpy",1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy",12,"F","Dark Brown","Goofy",1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL,4,"M","Brown","Man Eater",0);
