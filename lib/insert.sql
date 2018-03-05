c690f46e2adc9322dc31a594e3bf7cd9372f22ae
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-05 13:16:47 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate" , 10 ,"M" , "Brown" , "calm" , 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES("Rowdy" , 5 ,"M" , "Brown" , "ignorant" , 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha" , 14 ,"F" , "Brown" , "grumpy" , 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown" , 2 ,"M" , "Brown" , "curious" , 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa" , 6 ,"F" , "Brown" , "excited" , 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch" , 9 ,"M" , "Brown" , "sleepy" , 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy" , 13 ,"F" , "Brown" , "quite" , 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (null, 9 ,"F" , "Brown" , "aggressive" , 0);
