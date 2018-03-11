6c1b73d1e19d61829b55d478f4c46c6fe732a483
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-11 23:42:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", "1", "M", "Brown", "nice", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", "3", "M", "Red", "mean", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "2", "F", "Orange", "mean", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "5", "F", "Yellow", "nice", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "4", "F", "Tan", "nice", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", "2", "M", "Brown", "mean", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", "4", "F", "Pink", "nice", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null,"7", "F", "Purple",  "mean", "0");
