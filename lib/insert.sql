38c54dc388ac9c2862430008aa9704fbccc47731
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-03-25 18:27:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values ("Jerry", 55, "F", "black","cuddly", 1);
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values ("Harry", 3, "m", "blue","vegetarian", 1);
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values ("Barry", 5, "F", "orange","sheepish", 1);
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values ("Mary", 2, "m", "grey","sullen", 1);
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values ("Larry", 44, "F", "purple","cranky", 0);
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values ("Kerry", 53, "m", "white","depressed", 1);
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values (null, 22, "F", "brown","lonely", 1);
+INSERT INTO bears (name, age, gender, color,temperament, alive)
+  values ("Jim", 32, "F", "White","mean", 1);
