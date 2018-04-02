edd5b3763eb77782fc8105b3384ccbde772d87f3
Aspen <aspenjames@tqca.org>
2018-04-02 11:57:04 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 3, "F", "Brown", "Kind", 1),
+(2, "Rowdy", 6, "M", "Black", "Calm", 1),
+(3, "Tabitha", 8, "M", "Brown", "Quiet", 0),
+(4, "Sergeant Brown", 1, "F", "Black", "Friendly", 1),
+(5, "Melissa", 2, "M", "Brown", "Chatty", 0),
+(6, "Grinch", 7, "M", "Black", "Grumpy", 1),
+(7, "Wendy", 3, "M", "Brown", "Sleepy", 1),
+(8, NULL, 3, "F", "Brown", "Aggressive", 1);
