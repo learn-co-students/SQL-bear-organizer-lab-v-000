761247277b0831bbfa226095489d11b2c2222bf0
jpkim921 <jpkim921@gmail.com>
2018-04-27 17:03:11 -0400

    filled in sql_queries.rb

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,12 +1,14 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
-("Mr. Chocolate", 23, "M", "Red", "Friendly", 1),
-("Rowdy", 2, "M", "Orange", "Pleasant", 1),
+("Mr. Chocolate", 23, "M", "Red", "Friendly", 0),
+("Rowdy", 2, "M", "Orange", "Pleasant", 0),
 ("Tabitha", 5, "M", "Yellow", "Friendly", 1),
 ("Sergeant Brown", 8, "Green", "White", "Scary", 1),
-("Melissa", 1, "F", "Blue", "Friendly", 1),
+("Melissa", 1, "F", "Blue", "Friendly", 0),
 ("Grinch", 2, "F", "Indigo", "Selfish", 1),
 ("Wendy", 7, "F", "Violet", "Jealous", 1),
 (NULL, 4, "M", "Red", "Scared", 1);
+
+-- name, age, gender, color, temperament, alive
 
 
 -- Mr. Chocolate
