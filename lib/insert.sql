8dad74e01e52b1bac711fafe3eb312d314d9951a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-30 18:09:48 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 10, "M", "brown", "lazy", 0),
+(2,"Rowdy", 12, "F", "black", "energetic", 0),
+(3,"Tabitha", 2, "F", "light brown", "Curious", 1),
+(4,"Sargeant Brown", 1, "M", "amber", "restless", 0),
+(5,"Melissa", 9, "F", "black", "sullen", 0),
+(6,"Grinch", 2, "M", "dark brown", "grumpy", 1),
+(7,"Wendy", 16, "F", "light brown", "happy", 1),
+(8,null, 25, "M", "black", "mean", 0);
