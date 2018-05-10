693c8f98d2d540599b00f4fed5482fb67689f01e
Emmy White <emmalinerosewhite@gmail.com>
2018-05-10 12:54:10 -0400

    Halfway there.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "m", "Brown", "Aloof", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "m", "Chestnut", "Rambunctious", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha",13, "f", "Reddish-brown", "Sweet", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 23, "m", "Mahogany", "Serious", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "f", "Earthy", "Curious", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "m", "Light Brown", "Surly", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "f", "Chestnut", "Serious", 0);
+
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (7, "m", "Chestnut", "Rambunctious", 1);
