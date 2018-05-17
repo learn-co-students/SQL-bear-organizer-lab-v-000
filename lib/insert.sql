de4e80a7dc917ba5c342b0e6240375fb3afaaed5
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-05-16 17:32:21 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(1, "Mr. Chocolate", 5, "M", "brown", "easy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(2, "Rowdy", 10, "M", "brown", "sad", 2);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(3, "Tabitha", 7, "F", "brown", "sassy", 1 );
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(4, "Sergeant Brown", 11, "F", "black", "easy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(5, "Melissa", 2, "M", "white", "easy", 2);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(6, "Grinch", 3, "F", "black", "fire", 2);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(7, "Wendy", 5, "M", "brown", "sassy", 2);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(8, NULL, 1, "M", "white", "easy", 1);
+
+
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
