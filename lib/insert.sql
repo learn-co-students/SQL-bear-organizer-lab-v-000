47a4a7ba5bccfa2c8e1348ffedffacb599d69cf7
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-20 22:40:14 -0400

    Moving along with progress.  got a few select queries to write and then on to the next lab.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears ( 
+	name, 
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+	)
+VALUES 
+	('Mr. Chocolate', 29, 'M', 'Brown', 'Bouncing off the walls', 0),
+	('Rowdy', 10, 'M', 'White', 'Assertive', 1),
+	('Tabitha', 6, 'F', 'Brown', 'Friendly', 1),
+	('Sergeant Brown', 21, 'M', 'Blue', 'Shell shocked', 1),
+	('Melissa', 13, 'F', 'Brown', 'Playful', 1),
+	('Grinch', 99, 'M', 'Black', 'Careless', 1),
+	('Wendy', 6, 'F', 'White', 'Ha Ha Funny', 0),
+	(NULL, 10, 'M', 'White', 'Jealous', 0);
