b433e7029be3dd3112ae8b406618fb8ed34c1093
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-22 01:34:45 -0400

    All tests passing.  Ready for next lab.

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,11 +7,11 @@ INSERT INTO bears (
 	alive
 	)
 VALUES 
-	('Mr. Chocolate', 29, 'M', 'Brown', 'Bouncing off the walls', 0),
-	('Rowdy', 10, 'M', 'White', 'Assertive', 1),
-	('Tabitha', 6, 'F', 'Brown', 'Friendly', 1),
-	('Sergeant Brown', 21, 'M', 'Blue', 'Shell shocked', 1),
-	('Melissa', 13, 'F', 'Brown', 'Playful', 1),
-	('Grinch', 99, 'M', 'Black', 'Careless', 1),
-	('Wendy', 6, 'F', 'White', 'Ha Ha Funny', 0),
-	(NULL, 10, 'M', 'White', 'Jealous', 0);
+	('Mr. Chocolate', 20, 'M', 'dark brown', 'Nice', 0),
+	('Rowdy', 10, 'M', 'black', 'Intense', 1),
+	('Tabitha', 6, 'F', 'dark brown', 'Calm', 1),
+	('Sergeant Brown', 19, 'M', 'Green', 'Slimy', 0),
+	('Melissa', 13, 'F', 'dark brown', 'goofy', 1),
+	('Grinch', 2, 'M', 'Black', 'Grinchy', 1),
+	('Wendy', 6, 'F', 'Blue', 'Ha Ha Funny', 1),
+	(NULL, 20, 'M', 'Black', 'Aggressive', 0);
