50b06d0d8ac8f31130c03bfd8163cec1a26e44ef
Emily S <emily.sugano@gmail.com>
2018-05-30 21:46:20 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Adam", 36, "Male", "Brown", "Angry", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Emily", 34, "Female", "Brown", "Kind", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Caleb", 4, "Male", "Brown", "Angry", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("David", 1, "Male", "Brown", "Angry", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Yogi", 60, "Male", "Brown", "Kind", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Berstein", 66, "Male", "Brown", "Kind", "No");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("MamaBear", 100, "Female", "Brown", "Kind", "No");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null, 1, "Female", "Brown", "Kind", "Yes");
