31d4cf092aa2a704a2ce428b02cef8c5f0afb011
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-01 17:04:09 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 11, "M", "Brown", "Nice", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 8, "M", "White", "Agressive", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 4, "F", "Black", "Cool", 1);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 12, "M", "Brown", "Cool", 1);
+INSERT INTO bears VALUES (5, "Melissa", 9, "F", "Black", "Intense", 0);
+INSERT INTO bears VALUES (6, "Grinch", 12, "M", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (7, "Wendy", 10, "F", "Blue", "Playful", 1);
+INSERT INTO bears VALUES (8, null, 10, "M", "Black", "Agrressive", 0);
