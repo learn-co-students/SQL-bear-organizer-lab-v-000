af7c5e22937b9f9296ba07a26706560647ff5946
Alex Zhang <alexgzhang92@gmail.com>
2018-06-11 01:25:41 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Alex", 26, "M", "blue", "serious", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Patrick", 27, "M", "red", "mellow", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Lexi", 26, "F", "yellow", "caring", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Tommy", 26, "M", "purple", "dark", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Nicole", 27, "F", "green", "happy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Jide", 27, "M", "black", "jovial", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Ryan", 27, "M", "orange", "indifferent", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 44, "M", "white", "???", 1);
