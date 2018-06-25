200ad3f62a4000c2d65e631e22e0bf90b52d7d78
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-25 21:38:14 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,9 @@
-"CREATE TABLE bears (
+CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender char(1)(The choices would be "M" or "F")
-)"
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive, BOOLEAN
+)
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Yellow", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "F", "Black", "Mean", 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "M", "Red", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "Orange", "Mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "M", "Black", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "F", "Brown", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "F", "Pink", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9, "F", "Red", "Friendly", 2);
