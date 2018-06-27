b43bdc5d1f37f865789e129c9f1c6eb5d72b0e9f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-27 05:31:51 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,25 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Mr. Chocolate', 19, 'F', "polar", "mixed", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Rowdy', 75, 'M', "brown", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Tabitha', 6, 'M', "black", "rowdy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Sergeant Brown', 11, 'F', "off-white", "mousey", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Melissa', 11, 'M', "dark brown", "good", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Grinch', 44, 'F', "purple", "conflicted", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Wendy', 1, 'M', "green", "fine", 1);
-INSERT INTO bears (age, gender, color, temperament, alive)
-    VALUES (3, 'M', "eggshell", "angry", 0);
-
-
-
-
-
-
-
-
-
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(1, 'Mr. Chocolate', 19, 'F', "polar", "mixed", 1),
+(2, 'Rowdy', 75, 'M', "brown", "calm", 1),
+(3, 'Tabitha', 6, 'M', "black", "rowdy", 1),
+(4, 'Sergeant Brown', 11, 'F', "off-white", "mousey", 1),
+(5, 'Melissa', 11, 'M', "dark brown", "good", 1),
+(6, 'Grinch', 44, 'F', "purple", "conflicted", 1),
+(7, 'Wendy', 1, 'M', "green", "fine", 1),
+(8, null, 3, 'M', "eggshell", "angry", 0);
