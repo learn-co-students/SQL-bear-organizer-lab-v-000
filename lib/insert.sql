81feb926a38c36552c86ad8d3728090647204826
Dani Schuhman <dani8439@aol.com>
2018-07-27 20:20:52 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Sweet", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Black", "Playful", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Brown", "Affectionate", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "Brown", "Dictatorial", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "F", "White", "Calm", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Grumpy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "F", "Brown", "Lethargic", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( NULL, 3, "M", "White", "Vicious", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Black", "Rambunctious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 42, "F", "Purple", "Cranky", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 16, "M", "Brown", "Imperious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "White", "Fluffy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "M", "Green", "Mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "F", "Black", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "M", "Blue", "Belligerent", 0);
