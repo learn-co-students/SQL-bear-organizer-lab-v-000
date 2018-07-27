f356c47f48ac8b4c5eef13b6ca551eeb7ad518d7
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-27 21:54:31 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+--INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (name, age, color) VALUES ('Mr. Chocolate', 2, 'red');
+INSERT INTO bears (name, age, color) VALUES ('Rowdy', 1, 'blue');
+INSERT INTO bears (name, age, color) VALUES ('Tabitha', 5, 'orange');
+INSERT INTO bears (name, age, color) VALUES ('Sergeant Brown', 9, 'blue');
+INSERT INTO bears (name, age, color) VALUES ('Melissa', 7, 'purple');
+INSERT INTO bears (name, age, color) VALUES ('Grinch', 3, 'yellow');
+INSERT INTO bears (name, age, color) VALUES ('Wendy', 10, 'gold');
+INSERT INTO bears (name, age, color) VALUES (NULL, 2, 'white');
