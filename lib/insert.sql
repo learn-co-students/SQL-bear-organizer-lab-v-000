3b9cc4c18d3750724fd4544b92f377688892b222
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-02 14:54:22 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+  VALUES ('Mr. Chocolate', 4, 'M', 'Dark Brown', 'Relaxed', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+  VALUES ('Rowdy', 6, 'M', 'Dark Brown', 'Relaxed', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+  VALUES ('Tabitha', 3, 'F', 'Dark Brown', 'Relaxed', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+  VALUES ('Sergeant Brown', 7, 'M', 'Dark Brown', 'Relaxed', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+  VALUES ('Melissa', 3, 'F', 'Dark Brown', 'Relaxed', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+  VALUES ('Grinch', 8, 'M', 'Dark Brown', 'Relaxed', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+  VALUES ('Wendy', 4, 'F', 'Dark Brown', 'Relaxed', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 2, 'M', 'Dark Brown', 'Relaxed', 1);
