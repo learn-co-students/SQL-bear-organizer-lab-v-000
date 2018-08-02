e3cfd1fabb923f2835c6d545d0123e94196eeb4c
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-02 13:31:29 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sleepy', 4, 'M', 'Dark Brown', 'Relaxed', 1)
