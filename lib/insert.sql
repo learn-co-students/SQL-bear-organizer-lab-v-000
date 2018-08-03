eaf0aabc42cfe3f70c3ccfc17d310146739b75e0
Kazumi Karbowski <kazkazinus@gmail.com>
2018-08-02 23:11:53 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, 1, "Black", "Melancholic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, 1, "Brown", "Sanguine", 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, 2, "Black", "Melancholic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, 1, "Black", "Choleric", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 7, 2, "Black", "Phlegmatic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, 1, "Brown", "Sanguine", 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, 2, "Black", "Melancholic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, 1, "Black", "Sanguine", 1);
