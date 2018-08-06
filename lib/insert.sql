7c59a8a005824718ab48b60ef91128395b98f27a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-06 23:00:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears(name) values("Mr.Chocolate");
+insert into bears(name) values("Rowdy");
+insert into bears(name) values("Tabitha");
+insert into bears(name) values("Sergeant Brown");
+insert into bears(name) values("Melissa");
+insert into bears(name) values("Grinch");
+insert into bears(name) values("Wendy");
+insert into bears(name, age) values(NULL, "32");
