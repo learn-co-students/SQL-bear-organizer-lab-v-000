663c6a0d24490a38478c023a30b7ce87683e1d08
Will Schultz <will.j.schultz@gmail.com>
2018-08-06 14:03:27 +0000

    safety save

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 2, "m", "brown", "nice", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 3, "f", "brown", "nice", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 8, "F", "brownish", "nice", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 3, "m", "brown", "pretty nice", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 3, "m", "brown", "nice", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 2, "f", "brown", "nice", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 2, "f", "brown", "nice", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 2, "m", "brown", "not nice", 1);
