1238ea6aaa7c915ec4676a57a905228153ab8c86
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-07 06:09:20 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-insert into bears(name) values("Mr.Chocolate");
-insert into bears(name) values("Rowdy");
-insert into bears(name) values("Tabitha");
-insert into bears(name) values("Sergeant Brown");
-insert into bears(name) values("Melissa");
-insert into bears(name) values("Grinch");
-insert into bears(name) values("Wendy");
-insert into bears(name, age) values(NULL, "32");
+insert into bears(name, age, gender, color, temperament, alive) values("Mr.Chocolate", 5, "f", "red", "goofy", 1);
+insert into bears(name, age, gender, color, temperament, alive) values("Rowdy", 4, "m", "brown", "happy", 1);
+insert into bears(name, age, gender, color, temperament, alive) values("Tabitha", 3, "f", "black", "funny", 0);
+insert into bears(name, age, gender, color, temperament, alive) values("Sergeant Brown", 6, "m", "orange", "goofy", 1);
+insert into bears(name, age, gender, color, temperament, alive) values("Melissa", 2, "f", "brown", "silly", 1);
+insert into bears(name, age, gender, color, temperament, alive) values("Grinch", 5, "m", "grey", "droopy", 1);
+insert into bears(name, age, gender, color, temperament, alive) values("Wendy", 3, "f", "brown", "funny", 0);
+insert into bears(name, age, gender, color, temperament, alive) values(NULL, 6, "m", "black", "happy",1);
