fc7c53668baadc0d9ae11a8b74b46cfca6034bd7
hkato4188 <hkato@sas.upenn.edu>
2018-08-19 13:39:46 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 24, "M", "black", "gentle", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Rowdy", 8, "F", "brown", "rambunctious", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Tabitha", 34, "F", "spotted", "curious", "0");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Sergeant Brown", 45, "M", "black", "tough", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Melissa", 2, "F", "orange", "scared", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Grinch", 7, "M", "yellow", "mean", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Wendy", 23, "F", "blue", "happy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values (null, null, null, "brown", "dangerous", "0");
