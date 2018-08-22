e2b07bb473d4995dec73ae2364f256319e637571
Amit Singh <amit_nyc@live.com>
2018-08-22 03:33:48 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('MR. Chocolate', 4, 'M, Brown, Happy, Yes');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, 'F, Black, Rowdy, Yes');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 9, 'F, Brown, Happy, No');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M, Brown, Strict, Yes');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 1, 'F, Blue, Angry, Yes');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 4, 'M, Brown, Grudge, No');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 12, 'F, Brown, Happy, Yes');
-INSERT INTO bears (age, gender, color, temperament, alive) VALUES (4, 'M, Brown, Happy, Yes');
+INSERT INTO bears (name) VALUES ('MR. Chocolate');
+INSERT INTO bears (name) VALUES ('Rowdy');
+INSERT INTO bears (name) VALUES ('Tabitha');
+INSERT INTO bears (name) VALUES ('Sergeant Brown');
+INSERT INTO bears (name) VALUES ('Melissa');
+INSERT INTO bears (name) VALUES ('Grinch');
+INSERT INTO bears (name) VALUES ('Wendy');
+INSERT INTO bears (name) VALUES (NUll);
