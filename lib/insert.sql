002024a98ca411be3afea83365027cf3a9f0ef4b
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-08-28 11:45:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 5, 'M', 'brown', 'gentle', 1);
+
+INSERT INTO bears VALUES (2, 'Rowdy', 6, 'M', 'brown', 'ferocious', 0);
+
+INSERT INTO bears VALUES (3, 'Tabitha', 5, 'F', 'brown', 'gentle', 0);
+
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 5, 'M', 'brown', 'gentle', 1);
+
+INSERT INTO bears VALUES (5, 'Melissa', 5, 'F', 'brown', 'gentle', 1);
+
+INSERT INTO bears VALUES (6, 'Grinch', 9, 'F', 'brown', 'gentle', 1);
+
+INSERT INTO bears VALUES (7, 'Wendy', 29, 'F', 'brown', 'gentle', 1);
+
+INSERT INTO bears VALUES (8, null, 10, 'F', 'brown', 'protective', 1);
