67d354ec1ad16982296eed8cbbb104fe0078fc45
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-04 15:00:19 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+/* this is a comment. here will be your notes.
+This is how to create a table.
+
+CREATE TABLE table_name (
+
+STUFF seperated by commas
+
+);  <- always ends with semicolon
+
+*/
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender CHAR,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
