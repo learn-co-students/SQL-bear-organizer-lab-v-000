191de2b7b4a1bd8630078ef817f61270f7633e54
Harley Rayner <harleymteden@gmail.com>
2018-10-15 20:39:08 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+  VALUES ('Mr. Chocolate', 3, "male", "brown", "chilly", "yes", 'grizzly');
+
+  INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+    VALUES ('Rowdy', 8, "male", "white", "chilly", "yes", 'polar');
+
+    INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+      VALUES ('Tabitha', 1, "female", "brown", "chilly", "yes", "grizzly");
+
+      INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+        VALUES ('Seargeant Brown', 2, "male", "white / black", "chilly", "yes", 'panda');
+
+        INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+          VALUES ('Melissa', 14, "female", "brown", "chilly", "yes", 'grizzly');
+
+          INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+            VALUES ('Grinch', 99, "female", "white / black", "chilly", "yes", 'panda');
+
+            INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+              VALUES ('Wendy', 3, "female", "brown", "chilly", "yes", 'grizzly');
+
+              INSERT INTO bears (name, age, gender, color, temperament, alive, breed)
+                VALUES (NULL, 6, "female", "white", "chilly", "yes", "polar");
