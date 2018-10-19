90f18d825ad6f8117fbfc386e201fdc22f3e8435
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-19 05:22:02 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,6 +22,7 @@ def selects_most_prominent_color_and_returns_with_count
   #{}"SELECT MAX(color), COUNT(color) FROM bears"
   #{}"SELECT color, COUNT(*) FROM bears ORDER BY color DESC LIMIT 1"
   "SELECT MAX(color), COUNT() FROM bears;"
+  "SELECT color, COUNT() FROM bears WHERE count LIKE MAX(color) "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
