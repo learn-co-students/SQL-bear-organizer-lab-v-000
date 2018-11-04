828dbd372d376ce2ade39c5da817059e0f5b986b
meg-gutshall <meghan.gutshall@gmail.com>
2018-11-04 13:49:50 -0500

    Lots of bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "male", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 91, "male", "grey", "raucous", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "Black", "sassy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "female", "white", "chilly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "female", "purple", "bitchy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "male", "green", "grinchy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 18, "female", "yellow", "hyper", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, "male", "brown", "mean", 0);
