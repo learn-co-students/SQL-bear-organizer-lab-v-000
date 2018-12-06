fbed0267b02da2805e6badc1f5ad1a39d54d7772
Karen Flores <karen.ag.flores@gmail.com>
2018-12-06 19:24:13 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Mr. Chocoloate');
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Rowdy');
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Tabitha');
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Sergeant Brown');
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Melissa');
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Grinch');
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ('Wendy');
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL,);
