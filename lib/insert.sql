1ba58c63c40223bcb16a228640e0e6e5ed1cf608
Karen Flores <karen.ag.flores@gmail.com>
2018-12-12 03:12:37 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Mr. Chocoloate');
+  VALUES ('Mr. Chocoloate', 6, 'male', 'brown', 'sweet', 'yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Rowdy');
+  VALUES ('Rowdy', 2, 'male', 'black', 'energetic', 'yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Tabitha');
+  VALUES ('Tabitha', 4, 'female', 'brown', 'calm', 'yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Sergeant Brown');
+  VALUES ('Sergeant Brown', 7, 'male', 'black', 'demanding', 'no');
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Melissa');
+  VALUES ('Melissa', 8, 'female', 'brown', 'calm', 'no');
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Grinch');
+  VALUES ('Grinch', 5, 'male', 'black', 'grumpy', 'no');
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ('Wendy');
+  VALUES ('Wendy', 3, 'female', 'brown', 'spacey', 'yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES (NULL,);
+  VALUES (NULL, 2, 'male', 'black', 'grumpy', 'no');
