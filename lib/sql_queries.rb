1b7be0d7756837cad38850f0bb5326c3684128ca
Ashley Wong <ashwong04@gmail.com>
2018-12-16 16:16:47 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears"
+  "SELECT color, COUNT (*) FROM bears GROUP BY color ORDER BY 2 DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears WHERE temperament = "goofy";"
+  # "SELECT COUNT(temperament) FROM bears WHERE temperament = "goofy";"
 end
 
 def selects_bear_that_killed_Tim
