9288d828f90020983951bb7f207d90614782513b
Vince Andrew Pimentel <contact@vincep.me>
2018-12-19 21:47:08 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate",
+  3,
+  "male",
+  "brown",
+  "choleric",
+  1
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Rowdy",
+  4,
+  "male",
+  "black",
+  "phlegmatic",
+  1
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Tabitha",
+  5,
+  "female",
+  "white",
+  "sanguine",
+  1
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Sergeant Brown",
+  6,
+  "male",
+  "white",
+  "melancholic",
+  1
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Melissa",
+  7,
+  "female",
+  "brown",
+  "choleric",
+  0
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Grinch",
+  8,
+  "male",
+  "black",
+  "melancholic",
+  1
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Wendy",
+  9,
+  "female",
+  "brown",
+  "choleric",
+  1
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  NULL,
+  "10",
+  "female",
+  "brown",
+  "choleric",
+  1
+);
