0bcc58e8a2580e32433c16cdbf5d01fd6ed89507
Jonathan Dust <d24dust@hotmail.com>
2019-01-10 18:05:54 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(NULL, "Mr. Chocolate", 201, "male", "Chocolate", "angry without chocolate", true);
+INSERT INTO bears VALUES(NULL, "Rowdy", "-4", "male", "white", "noice", true);
+INSERT INTO bears VALUES(NULL, "Tabitha", "10", "female", "purple", "nice", true);
+INSERT INTO bears VALUES(NULL, "Sergeant Brown", 90, "male", "navy blue", "A fighter", false);
+INSERT INTO bears VALUES(NULL, "Melissa", 21, "female", "light purple", "meany", false);
+INSERT INTO bears VALUES(NULL, "Grinch", 120, "male", "green", "a grump around christmas", true);
+INSERT INTO bears VALUES(NULL, "Wendy", 25, "female", "black", "friendly", true);
+INSERT INTO bears VALUES(NULL, NULL, NULL, NULL, "Grizzly", "Nice bear, didn't like Tim, f", false);
