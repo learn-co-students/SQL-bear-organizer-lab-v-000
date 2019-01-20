57e62d036f30a6b5f18c3c347c7ff9accafd928f
Sharon Rieger <sharonarieger@gmail.com>
2019-01-20 10:58:10 -0500

    create bears table, add columns

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+/* where you create your schema*/
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name  TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+
+
+
+
+
+
+
