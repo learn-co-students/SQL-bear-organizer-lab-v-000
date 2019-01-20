97b13c06cc74781f37e90d4f1941a1969e396c64
Ashley Bland <ashleyjbland@gmail.com>
2019-01-20 18:12:48 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", 11, "male", "brown", "friendly", 1);
+    VALUES ("Mr. Chocolate", 11, "M", "brown", "friendly", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Rowdy", 3, "male", "black", "playful", 1);
+    VALUES ("Rowdy", 3, "M", "black", "playful", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Tabitha", 34, "female", "white", "warm", 0);
+    VALUES ("Tabitha", 34, "F", "white", "warm", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Sergeant Brown", 47, "male", "brown", "stern", 0);
+    VALUES ("Sergeant Brown", 47, "M", "brown", "stern", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Melissa", 12, "female", "black", fine, 1);
+    VALUES ("Melissa", 12, "F", "black", "fine", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Grinch", 4, "male", "green", "mischievous", 1);
+    VALUES ("Grinch", 4, "M", "green", "mischievous", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Wendy", 8, "female", "golden", "sweet", 1);
+    VALUES ("Wendy", 8, "F", "golden", "sweet", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES (NULL, 10, "female", "red", "distructive", 1);
+    VALUES (NULL, 10, "F", "red", "distructive", 1);
