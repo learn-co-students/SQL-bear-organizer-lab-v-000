a74c28b612ba79ed92ba9183fe73cebe2ec415e0
Sharon Rieger <sharonarieger@gmail.com>
2019-01-21 09:31:50 -0500

    SELECT oldest bear, return name and age

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,11 +8,12 @@ end
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT bears.name, bears.age FROM bears WHERE alive = 1 ORDER BY bears.age ASC ";
+
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT bears.name, bears.age FROM bears ORDER BY bears.age DESC LIMIT 1" ;
 end
 
 def select_youngest_bear_and_returns_name_and_age
