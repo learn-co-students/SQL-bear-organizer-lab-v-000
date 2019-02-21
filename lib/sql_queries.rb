f87d882e40346a09e4c4783d88887ba58718a32f
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-02-21 23:49:15 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears ORDER BY age ASC"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE age = (SELECT MAX(age) FROM bears)"
+  "SELECT name, age FROM bears WHERE age = (SELECT MAX(age) FROM bears) AND name IS NOT NULL"
 end
 
 def select_youngest_bear_and_returns_name_and_age
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name IS NULL"
 end
