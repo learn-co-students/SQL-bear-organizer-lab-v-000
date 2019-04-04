b42c7a3b5b4e9f7e484a7fe589997c777d52c133
NaKia Whitby <whitby.nakia@gmail.com>
2019-04-04 21:44:38 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 2, 'M', 'dark brown', 'jovial', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 5, 'M', 'cream', 'mild', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 20, 'F', 'black', 'goofy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 32, 'M', 'gray', 'aggressive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 30, 'F', 'white', 'flighty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 15, 'M', 'black', 'mellow', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 17, 'F', 'brown', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 24, 'F', 'white', 'timid', 0);
