9d10a688467f457fbf36e1e3b3cba8d5eb6b42a5
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-19 19:50:32 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER /*Boolean values are stored as integers 0 (false) and 1 (true).*/
+);
---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 21, "Male", "Black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "Female", "Black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 45, "Male", "Black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 54, "Female", "Brown", "happy", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 132, "Male", "Brown", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 54, "Female", "Brown", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 74, "Male", "Brown", "happy", 1);
