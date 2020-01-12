b4c6224a89c0a836bf58a297f6605f8466f02466
Falgoon Patel <falgoonit@gmail.com>
2020-01-12 00:39:13 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 12, 'M', 'dark brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 8, 'M', 'dark brown', 'Excitable', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 6, 'F', 'white', 'Calm', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 12, 'M', 'dark brown', 'Irritable', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 13, 'F', 'white', 'Calm', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 22, 'M', 'White', 'Angry', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 6, 'F', 'black', 'Calm', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 20, 'M', 'black', 'aggressive', 0);
